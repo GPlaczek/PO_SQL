1ec2b224f02dffebdecf8938523694a0228f24a1
grzegorz.placzek <grzegorz.placzek@student.put.poznan.pl>
2022-01-08 14:24:33 +0100

    Add Search Tables class

--- Controllers/ResultsController.cs
+++ Controllers/ResultsController.cs
@@ -3,18 +3,18 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using PO_SQL.Models.ActionClasses;
 
 namespace PO_SQL.Controllers
 {
     public class ResultsController : Controller
     {
+        private IAction a1;
         [HttpPost]
-        public IActionResult SearchResult(string productName, string productDescription, string priceMin, string priceMax)
+        public IActionResult SearchResults(string productName, string productDescription, string priceMin, string priceMax)
         {
-            ViewData["ProductName"] = productName;
-            ViewData["ProductDescription"] = productDescription;
-            ViewData["PriceMin"] = priceMin;
-            ViewData["PriceMax"] = priceMax;
+            a1 = new SearchTables(productName, productDescription, priceMin, priceMax, new List<string>() { "tabela" });
+            ViewData["query"] = a1.execute();
             return View();
         }
         public IActionResult AddResult(string productName, string productDescription, string price)
---
+++ Models/ActionClasses/SearchTables.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PO_SQL.Models.ActionClasses
+{
+    public class SearchTables : IAction
+    {
+        private string Name { get; set; }
+        private string Desc { get; set; }
+        private string PriceMin { get; set; }
+        private string PriceMax { get; set; }
+        public List<String> Tables;
+        public string execute()
+        {
+            return $"SELECT * FROM {Tables[0]} WHERE name={Name} and desc=*{ Desc}* and price > {PriceMin} and price < {PriceMax}";
+        }
+        public SearchTables(string Name, string Desc, string min, string max, List<String> Tables)
+        {
+            this.Name = Name;
+            this.Desc = Desc;
+            this.PriceMin = min;
+            this.PriceMax = max;
+            this.Tables = Tables;
+        }
+    }
+}
